7615ea1d59094c4abaeccfdd659e003f00395996
Nathan-hub123 <nathantheovie@gmail.com>
2026-08-12 12:50:36 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+print("================================")
+print("     QUOTATION MANAGEMENT")
+print("================================")
+
+print("1. Create quotation")
+print("2. View quotations")
+print("3. Exit")
+
