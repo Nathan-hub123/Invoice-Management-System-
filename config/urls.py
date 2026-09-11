e39dbebdfd56b8c271a1f986ac1b9ee5e692087a
Nathan-hub123 <nathantheovie@gmail.com>
2026-09-11 12:01:40 +0100

    Add API home endpoint

--- config/urls.py
+++ config/urls.py
@@ -28,6 +28,7 @@ def home(request):
 
 
 urlpatterns = [
+    path("", home),
     path('admin/', admin.site.urls),
     path("api/auth/", include("users.urls")),
     path("api/customers/", include("customers.urls")),
